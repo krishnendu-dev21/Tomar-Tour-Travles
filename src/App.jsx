c10b8117e04b965ee6bab5e65dc911b62c2315dc
Krishnendu Ghosh <Krishnendughosh.2105@gmail.com>
2026-09-07 19:09:31 +0530

    Fix Contact import

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Home from "./pages/home";
 import About from "./pages/about";
-import Contact from "./pages/contact";
+// import Contact from "./pages/contact";
+import Contact from './pages/Contact'
 import Footer from './components/footer';
 import ScrollToTop from "./components/scrollToTop";
 function App() {
