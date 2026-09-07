6a86b43faac9c72f29d6086c227054303cb7040f
Krishnendu Ghosh <Krishnendughosh.2105@gmail.com>
2026-09-07 19:00:46 +0530

    Fix Netlify build

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
+import Navbar from "./components/navbar";
+import Home from "./pages/home";
 import About from "./pages/about";
-import Contact from "./pages/Contact";
+import Contact from "./pages/contact";
 import Footer from './components/footer';
 import ScrollToTop from "./components/scrollToTop";
 function App() {
